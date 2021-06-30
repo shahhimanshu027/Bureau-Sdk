66c47dd132c3196a7c44551899d72ae93306ef04
shah <shahhimanshu02@gmail.com>
2021-06-30 16:32:21 +0530

    Done : - minor changes

--- bureau_sdk/src/main/java/com/bureau/helpers/NotificationHelper.kt
+++ bureau_sdk/src/main/java/com/bureau/helpers/NotificationHelper.kt
@@ -3,7 +3,9 @@ package com.bureau.helpers
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.media.RingtoneManager
 import android.net.Uri
@@ -15,15 +17,19 @@ import com.bureau.utils.NOTIFICATION_CHANNEL_ID
 import com.bureau.utils.NOTIFICATION_CHANNEL_NAME
 import java.util.*
 
+
 class NotificationHelper {
 
     fun showNotification(context: Context, title: String?, description: String?) {
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 //        val intent = Intent(context, defaultActivity::class.java).apply {
 //            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 //        }
-//        val pendingIntent: PendingIntent? = PendingIntent.getActivity(context, 0, intent,0)
+        val intent: Intent? =
+            context.packageManager.getLaunchIntentForPackage("com.devstory.bureau")
+        val pendingIntent: PendingIntent? = PendingIntent.getActivity(context, 0, intent, 0)
         val mNotificationCompatBuilder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
                 NOTIFICATION_CHANNEL_NAME,
@@ -40,11 +46,11 @@ class NotificationHelper {
                 .setContentText(description)
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher_round)
-//                .setContentIntent(pendingIntent)
+                .setContentIntent(pendingIntent)
         } else {
             commonNotification(
                 context, title,
-                description, defaultSoundUri!!
+                description, pendingIntent!!, defaultSoundUri!!
             )
         }
         val uniqueNotificationId = (Date().time / 1000L % Int.MAX_VALUE).toString().toInt()
@@ -54,7 +60,7 @@ class NotificationHelper {
     private fun commonNotification(
         context: Context,
         title: String?,
-        messageBody: String?/*, pendingIntent: PendingIntent*/,
+        messageBody: String?, pendingIntent: PendingIntent,
         defaultSoundUri: Uri
     ): NotificationCompat.Builder {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -66,7 +72,7 @@ class NotificationHelper {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher_round)
-//                .setContentIntent(pendingIntent)
+                .setContentIntent(pendingIntent)
                 .setSound(defaultSoundUri).setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
         } else {
             NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
@@ -75,7 +81,7 @@ class NotificationHelper {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher_round)
-//                .setContentIntent(pendingIntent)
+                .setContentIntent(pendingIntent)
         }
     }
 }
